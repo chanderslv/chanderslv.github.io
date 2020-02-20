6ecc2b6bf25c4e08b7755935381ab6f80cf23dec
Ivo <chanderslv@gmail.com>
2020-02-20 19:19:30 +0000

    mysql

--- w4l1/js/index.js
+++ w4l1/js/index.js
@@ -1,48 +1,25 @@
 $(function() {
-    var $makeRequest = $('.make-request')
-    var page = 1;
-    var progressInterval;
-
+    console.log($('#data-table').find('tbody'))
     $('.make-request').on('click', function() {
         $.ajax({
             method: "GET",
-            url: "https://reqres.in/api/users?page=" + page
+            url: "https://reqres.in/api/users?page="
         }).done(function(response) {
-            response.data.forEach(function(value, index, arr) {
-                const img = "<img src=\""+value.avatar+"\">"
+            response.data.forEach(function(value, index) {
                 const row = "\
                 <tr>\
                     <td>"+value.id+"</td>\
-                    <td class=\"cell-img\">"+img+"</td>\
+                    <td>"+value.img+"</td>\
                     <td>"+value.first_name+"</td>\
                     <td>"+value.last_name+"</td>\
                     <td>"+value.email+"</td>\
                 </tr>";
 
-                $('#data-table').find('tbody').append(row)
-                               
+                $('#data-table').find('tbody')
+                .append(row)
             })
-            page ++;
-
-            $makeRequest.attr('disabled', true);
         })
-   })
-   
-   $('.start-action').on('click', startProgress)
-    
-   function startProgress() {
-       const cycleInterval = $('#cycle-interval').val()
-       let width = 0
+    })
 
-       if (cycleInterval > 0) {
-           $('.progress').width(0);
-
-           progressInterval = setInterval(function() {
-               width ++;
-               $('progress').width(width + "%");
-           }, 100)
-
-           }
-       }
-   }
-)
+    var makeRequest = function() {}
+})
